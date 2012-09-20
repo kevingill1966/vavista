07d92fd9d568e484a297d6e1da202730ce41ff4d
Kevin Gill <kevin@movieextras.ie>
2012-09-20 10:10:41 +0100

    Moved the vavista.M and vavista.rpc modules out to separate eggs.

--- src/vavista/rpc/README.txt
+++
@@ -1,2 +0,0 @@
-This code is lifted directly from FMQL (caregraf.org). It is put
-here to make it easier for me to reuse.
--- src/vavista/rpc/__init__.py
+++
@@ -1,7 +0,0 @@
-# Simplistic API for calling RPCs with a Vista Server
-
-from brokerRPCLocal import VistARPCConnection, PLiteral, PList, PReference, PEncoded
-
-def connect(host, port, access, verify, context, debug=False):
-    return VistARPCConnection(host, port, access, verify, context)
-
